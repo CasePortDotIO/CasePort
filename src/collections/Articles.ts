e3fca4e5a3e43e20c6741e0bd0b08db5b7366898
Sonain-Danish <108166419+Sonain-Danish@users.noreply.github.com>
2026-05-12 14:30:53 +0500

    .

--- src/collections/Articles.ts
+++ src/collections/Articles.ts
@@ -887,7 +887,6 @@ export const Articles: CollectionConfig = {
             },
             { name: 'articleSection', type: 'text' },
             { name: 'apaCitation', type: 'text' },
-            { name: 'customJsonLd', type: 'textarea' },
           ],
         },
         {
